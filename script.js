58334b51d18771633a198610d4fad16574487237
eigodev <eigo_developer@outlook.com>
2024-09-02 16:03:28 -0300

    202409021600 - Login/Sign-up Buttons

--- script.js
+++ script.js
@@ -1,39 +1,15 @@
-const userCredentials = [{
-    username: "samoett",
-    email: "samoettinger_@outlook.com",
-    password: "@Senha847136"
-},
-{
-    username: "mayuka",
-    email: "carooolmayumi@gmail.com",
-    password: "090887"
-}]
+const passwordInput = document.getElementById('password')
+const showPasswordIcon = document.getElementById('show-icon')
+const hidePasswordIcon = document.getElementById('hide-icon')
 
-document.getElementById("loginForm").addEventListener('keydown', function(event){
-    console.log("key pressed:", event.key)
-    
-    if (event.key === 'Enter' || event.keyCode === 13){
-        event.preventDefault()
-        validateLogin(event)
-    }
+showPasswordIcon.addEventListener('click', function(){
+    passwordInput.type = 'text';
+    showPasswordIcon.style.display = 'none'
+    hidePasswordIcon.style.display = 'block'
 })
 
-function validateLogin(event){
-    const enteredUsername = document.getElementById('username').value
-    const enteredPassword = document.getElementById('password').value
-
-    let loginSuccesful = false
-    let matchedUser = null
-
-    for (let index = 0; userCredentials.length; index++){
-        let user = userCredentials[index]
-
-        if(enteredUsername === user.username || enteredUsername === user.email && enteredPassword === user.password){
-            loginSuccesful = true
-            matchedUser = user
-            break
-        }
-    }
-    if (loginSuccesful){alert(`Seja bem-vindo(a) ${matchedUser.username}`)}
-    else {alert('Você não possui cadastro!')}
-}
+hidePasswordIcon.addEventListener('click', function(){
+    passwordInput.type = 'password'
+    hidePasswordIcon.style.display = 'none'
+    showPasswordIcon.style.display = 'block'
+})
