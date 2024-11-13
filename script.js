85a58f6d12f1d975a4feae8b6abe35a43cce9f55
eigodev <eigo_developer@outlook.com>
2024-11-13 16:01:29 -0300

    131120241601 - Design update

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const passwordInput = document.getElementById('password')
 const showPasswordIcon = document.getElementById('show-icon')
 const hidePasswordIcon = document.getElementById('hide-icon')
+const logIn = document.querySelector('#login-in')
+const signUp = document.querySelector('#sign-up')
 
 showPasswordIcon.addEventListener('click', function(){
     passwordInput.type = 'text';
@@ -12,4 +14,12 @@ hidePasswordIcon.addEventListener('click', function(){
     passwordInput.type = 'password'
     hidePasswordIcon.style.display = 'none'
     showPasswordIcon.style.display = 'block'
+})
+
+logIn.addEventListener('click', function(){
+    console.log('clicou Login')
+})
+
+signUp.addEventListener('click', function(){
+    console.log('clicou Sign-up')
 })
